02cf442e54e4ca4754d5a3ebb9d58641203587ef
GoshaLetov <KhvoshchevKMwork@yandex.ru>
2021-11-27 05:25:26 +0300

    HW2_candles_cusum.Rmd first try & futures.R secind try

--- HW2/HW2_dividends.R
+++ HW2/HW2_dividends.R
@@ -1,8 +1,3 @@
-
-list.of.packages <- c("quantmod", 'magrittr', 'xts')
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
 library(quantmod)
 library(magrittr)
 library(xts)
@@ -15,12 +10,11 @@ from <- '1962-01-01'
 stocks <- 'IBM'
 
 # Загрузите цену закрытия (Close) акций IBM с 1962-01-01 по 2021-11-25 с помощью функции getSymbols 
-asset <- Lo(get(getSymbols(stocks, from = from, to = to)))
+asset <- Cl(get(getSymbols(stocks, from = from, to = to)))
 
 # Загрузите дивиденды по этой акции за тот же промежуток времени getDividends
 dividends <- getDividends(stocks, from = from, to = to)
 
 # Сделайте эджастмент цены на дивиденды используя difference метод из лекции
-adjusted_asset <- rowSums(na.fill(merge(asset, dividends, join = 'left'), 0))
+adjusted_asset <- asset - lag(sum(dividends) - cumsum(dividends), -1)
 plot(adjusted_asset - asset)
-
